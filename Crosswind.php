115bf99192c692d2c0457ad8e3ef73529a041b54
Kenneth P. Hough <kenneth@egtech.us>
2020-10-20 18:11:40 -0500

    Add db and account init functions

--- Crosswind.php
+++ Crosswind.php
@@ -51,6 +51,9 @@ if (isset($argv[1], $argv[2]) ) {
 
     // init db
     if ($argv[1] == 'init' && $argv[2] == 'db') {
+        // load DB lib
+        require_once __DIR__.'/lib/Database.php';
+
         // create db connection sh for convenience
         $content = <<<EOT
 #!/usr/bin/bash
@@ -67,14 +70,110 @@ EOT;
         echo sprintf("database %s created\n", KYTE_DB_DATABASE);
 
         echo "Creating tables...\n";
+        $model_sql = \Crosswind\Database::create_tables($crosswind_env['db_charset'], $crosswind_env['db_engine']);
+        $sql_stmt = '';
+
+        foreach($model_sql as $stmt) {
+            $sql_stmt .= $stmt."\n\n";
+        }
+        file_put_contents('~/schema.sql', $sql_stmt);
 
         // create tables
         shell_exec(sprintf("mysql -u%, -p%s -h%s %s < schema.sql", KYTE_DB_USERNAME, KYTE_DB_PASSWORD, KYTE_DB_HOST, KYTE_DB_DATABASE);
         // TODO: check return response
+
+        echo "DB initialization complete!\n\n";
+        echo "Next, consider running `{$argv[0]} init account` to create API keys, install default roles and permission and setup a Kyte account.\n";
     }
 
-    // init account
-    if ($argv[1] == 'init' && $argv[2] == 'account') {
+    // init account [Account Name] [User's Name] [email] [password]
+    if ($argv[1] == 'init' && $argv[2] == 'account' && isset($argv[3], $argv[4], $argv[5], $argv[6])) {
+        echo "Begining account initialization...\n\n";
+
+        // create account
+        echo "Creating account...";
+        $account = new \Kyte\ModelObject(Account);
+        $length=20; //maximum: 32
+        do {
+            $account_number = substr(md5(uniqid(microtime())),0,$length);
+        } while ($account->retrieve('number', $account_number));
+        
+        if (!$account->create([
+            'name'      => $argv[3],
+            'number'    => $account_number
+        ])) {
+            echo "FAILED\n\n";
+            exit(-1);
+        }
+        echo "OK\n\n";
+        echo "Account # $account_number created.\n\n";
+
+        echo "Creating API Keys...";
+        // create API Keys
+        $epoch = time();
+        $identifier = uniqid();
+        $secret_key = hash_hmac('sha1', $identifier, $epoch);
+        $public_key = hash_hmac('sha1', $identifier, $secret_key);
+
+        $apiKey = new \Kyte\ModelObject(APIKey);
+        if (!$apiKey->create([
+            'identifier' => $identifier
+            'public_key' => $public_key
+            'secret_key' => $secret_key
+            'epoch' => $epoch,
+            'kyte_account' => $account_number
+        ])) {
+            echo "FAILED\n\n";
+            exit(-1);
+        }
+        echo "OK\n\n";
+        echo "Identifier $identifier\n";
+        echo "Public Key $public_key\n";
+        echo "Secret Key $secret_key\n\n";
+
+        // populate with default Admin role for all models
+        echo "Creating new admin role...";
+        $role = new \Kyte\ModelObject(Role);
+        if (!$role->create([
+            'name' => 'Administrator',
+            'kyte_account' => $account_number
+        ])) {
+            echo "FAILED\n\n";
+            exit(-1);
+        }
+        echo "OK\n\n";
+
+        echo "Creating default admin permissions for role.\n";
+        foreach (KYTE_MODELS as $model) {
+            foreach (['new', 'update', 'get', 'delete'] as $actionType) {
+                $permission = new \Kyte\ModelObject(Permission);
+                echo "Creating $actionType permission for ".$$model['name']."...";
+                if (!$permission->create([
+                    'role'  => $role->getParam('id'),
+                    'model' => $$model['name'],
+                    'action' => $actionType,
+                    'kyte_account' => $account_number
+                ])) {
+                    echo "FAILED\n\n";
+                    exit(-1);
+                }
+                echo "OK\n\n";
+            }
+        }
+
+        // create user
+        echo "Creating new admin user...";
+        $role = new \Kyte\ModelObject(User);
+        if (!$role->create([
+            'name' => $argv[4],
+            'email' => $argv[5],
+            'password' => password_hash($argv[6], PASSWORD_DEFAULT),
+            'kyte_account' => $account_number
+        ])) {
+            echo "FAILED\n\n";
+            exit(-1);
+        }
+        echo "OK\n\n";
 
     }
 }
