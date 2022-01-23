7aff8589f4ca4c2e0544f9743a6ae186ad89c462
Kenneth Hough <kenneth@egtech.us>
2022-01-23 11:50:47 +0900

    Update format

--- lib/Controller.php
+++ lib/Controller.php
@@ -7,37 +7,37 @@ class Controller {
 		$php = <<<EOT
 <?php
 
-class $name extends \Kyte\Mvc\Controller\ModelController
+class {$name}Controller extends \Kyte\Mvc\Controller\ModelController
 {
 	/*
 	 * available controller flags that can be modified in the initialization hook
 	 * /
 	// protected \$user;
-    // protected \$account;
-    // protected \$session;
-    // protected \$response;
-    // public \$dateformat;
-    // public \$model;
-    // protected \$cascadeDelete;
-    // protected \$getFKTables;
-    // protected \$getExternalTables;
-    // protected \$requireAuth;
-    // protected \$requireRoles;
-    // protected \$requireAccount;
-    // protected \$failOnNull;
-    // protected \$allowableActions;
-    // protected \$checkExisting;
-    // protected \$exceptionMessages;
+	// protected \$account;
+	// protected \$session;
+	// protected \$response;
+	// public \$dateformat;
+	// public \$model;
+	// protected \$cascadeDelete;
+	// protected \$getFKTables;
+	// protected \$getExternalTables;
+	// protected \$requireAuth;
+	// protected \$requireRoles;
+	// protected \$requireAccount;
+	// protected \$failOnNull;
+	// protected \$allowableActions;
+	// protected \$checkExisting;
+	// protected \$exceptionMessages;
 
 	/*
 	 * available hooks to modify controller behaviour
 	 */
 	public function hook_init() {}
-    public function hook_auth() {}
-    public function hook_prequery(\$method, &\$field, &\$value, &\$conditions, &\$all, &\$order) {}
-    public function hook_preprocess(\$method, &\$r, &\$o = null) {}
-    public function hook_response_data(\$method, \$o, &\$r = null, &\$d = null) {}
-    public function hook_process_get_response(&\$r) {}
+	public function hook_auth() {}
+	public function hook_prequery(\$method, &\$field, &\$value, &\$conditions, &\$all, &\$order) {}
+	public function hook_preprocess(\$method, &\$r, &\$o = null) {}
+	public function hook_response_data(\$method, \$o, &\$r = null, &\$d = null) {}
+	public function hook_process_get_response(&\$r) {}
 
 	/*
 	 * alternatively, the following class methods can be overriden instead
